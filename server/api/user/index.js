cc8441c0a5358fb3f21b17a6a67ebdd01355bff3
Eric Chan <kakchan@gmail.com>
2015-02-21 21:16:24 +1100

    change naming convension - server side controller to router so that it will be easier for searching

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -1,17 +1,17 @@
 'use strict';
 
 var express = require('express');
-var controller = require('./user.controller');
+var user_router = require('./user.router');
 var config = require('../../config/environment');
 var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', auth.hasRole('admin'), controller.index);
-router.delete('/:id', auth.hasRole('admin'), controller.destroy);
-router.get('/me', auth.isAuthenticated(), controller.me);
-router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
-router.get('/:id', auth.isAuthenticated(), controller.show);
-router.post('/', controller.create);
+router.get('/', auth.hasRole('admin'), user_router.index);
+router.delete('/:id', auth.hasRole('admin'), user_router.destroy);
+router.get('/me', auth.isAuthenticated(), user_router.me);
+router.put('/:id/password', auth.isAuthenticated(), user_router.changePassword);
+router.get('/:id', auth.isAuthenticated(), user_router.show);
+router.post('/', user_router.create);
 
 module.exports = router;
