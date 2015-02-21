55a57fc0f2f3ce8fe18bb28a2126e1ec5f56a3da
Eric Chan <kakchan@gmail.com>
2015-02-21 14:03:34 +1100

    move the menu to the left handside of the screen

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,10 +2,21 @@
 
 angular.module('sendoApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+    $scope.menu = [
+      { 'title': 'Orders', 'link': '/orders', isShow: Auth.isAdmin },
+      { 'title': 'Customers', 'link': '/customers', isShow: Auth.isAdmin },
+      { 'title': 'Products', 'link': '/products', isShow: Auth.isAdmin },
+      { 'title': 'Collections', 'link': '/collections', isShow: Auth.isAdmin },
+      { 'title': "Blog Posts", "link": "blog_posts", isShow: Auth.isAdmin },
+      { 'title': "Pages", "link": "pages", isShow: Auth.isAdmin },
+      { "title": "Navigations", "link": "navigations", isShow: Auth.isAdmin },
+      { "title": "Themes", "link": "themes", isShow: Auth.isAdmin },
+      { "title": "Settings", "link": "settings", isShow: Auth.isAdmin }
+    ];
+
+    $scope.is_show_menu_item = function( menu_item ) {
+      return menu_item.type = Auth.isAdmin;
+    };
 
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
