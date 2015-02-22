072e38f79fd30804f27130d560a8f6eaf0c8d36b
Eric Chan <kakchan@gmail.com>
2015-02-22 17:32:35 +1100

    disable modules that are not developing

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,15 +3,21 @@
 angular.module('sendoApp')
   .controller('NavbarController', function ($scope, $location, Auth) {
     $scope.menu = [
+/*
       { 'title': 'Orders', 'link': '/admin/orders', isShow: Auth.isAdmin },
       { 'title': 'Customers', 'link': '/admin/customers', isShow: Auth.isAdmin },
+*/
       { 'title': 'Products', 'link': '/admin/products', isShow: Auth.isAdmin },
+/*
       { 'title': 'Collections', 'link': '/admin/collections', isShow: Auth.isAdmin },
       { 'title': "Blog Posts", "link": "/admin/blog_posts", isShow: Auth.isAdmin },
       { 'title': "Pages", "link": "/admin/pages", isShow: Auth.isAdmin },
       { "title": "Navigation", "link": "/admin/navigation", isShow: Auth.isAdmin },
+*/
       { "title": "Themes", "link": "/admin/themes", isShow: Auth.isAdmin },
+/*
       { "title": "Settings", "link": "/admin/settings", isShow: Auth.isAdmin }
+*/
     ];
 
     $scope.is_show_menu_item = function( menu_item ) {
